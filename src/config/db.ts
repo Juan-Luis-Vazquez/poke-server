807840c45633e686d76f2994b9da7a1dae5ce171
bruno7y62 <bruno7y62@gmail.com>
2024-05-05 17:28:00 -0600

    PS-11 Create remote database and connect main API

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 
 // For connection to hosted database
-//const DB = process.env.DATABASE?.replace(
-//    '<PASSWORD>',
-//    process.env.DATABASE_PASSWORD!
-//);
-
-const DB: string = process.env.DATABASE!;
+const DB = process.env.DATABASE!.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD!
+);
+// For connection to local database
+//const DB: string = process.env.DATABASE!;
 
 const db = async() => {
     try {
